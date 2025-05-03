e083970a7f0401563416c6e3df4582d91cd0a31e
Alex Darigan <alexdarigan@VFIEVOX3.Router>
2025-05-03 16:13:28 +0100

    Improve Tests

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -1,5 +1,12 @@
 
 // Example Function to Test
-export default function sum(a, b) {
-    return a + b;
+
+function add(a, b) {
+    return a + b
 }
+
+function subtract(a, b) {
+    return a - b
+}
+
+export { add, subtract }
