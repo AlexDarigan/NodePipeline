f79e2f196cfcced8cc19e81e387175341cd430eb
Alex Darigan <alexdarigan@VFIEVOX3.Router>
2025-05-03 15:52:22 +0100

    Add Tests

---
+++ src/calculator/calculator.js
@@ -0,0 +1,5 @@
+
+// Example Function to Test
+export default function sum(a, b) {
+    return a + b;
+}
---
+++ src/calculator/calculator.test.js
@@ -0,0 +1,6 @@
+// import sum from './calculator';
+import sum from "./calculator";
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
