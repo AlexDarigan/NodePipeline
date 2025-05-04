2e0b30c89b208587febb032e364ee5698a660740
Alex Darigan <alexdarigan@VFIEVOX3.Router>
2025-05-04 15:34:24 +0100

    test gcp-deploy

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import { createServer } from 'node:http';
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const PORT = process.env.PORT || 8080; // Use the port that GCP provides, or default to 8080
+
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Test Deploy From Workflow');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(PORT, () => {
+  console.log(`Server running at ${PORT}/`);
 });
