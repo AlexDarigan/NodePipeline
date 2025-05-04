867933430468491f21257b0f1e62022531301898
Alex Darigan <alexdarigan@VFIEVOX3.Router>
2025-05-04 16:28:28 +0100

    Add X

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import { createServer } from 'node:http';
+import fs from 'fs';
 
 const PORT = process.env.PORT || 8080; // Use the port that GCP provides, or default to 8080
 
-
 const server = createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Test Deploy From Workflow');
+  res.setHeader('Content-Type', 'text/html');
+  fs.readFile("./index.html", null, function(error, data) {
+    if(error) {
+      res.status(404).end("File not found!");
+    } else {
+      res.write(data);
+      res.end();
+    }
+  })
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, "127.0.0.1", () => {
   console.log(`Server running at ${PORT}/`);
 });
